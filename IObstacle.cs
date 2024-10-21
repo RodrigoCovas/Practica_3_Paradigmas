b47bd544ed4f5dfd920e34abee28c2ce1e518c88
Rodrigo Covas Vasquez <202210672@alu.comillas.edu>
2024-10-21 17:07:54 +0200

    Add project files.

---
+++ IObstacle.cs
@@ -0,0 +1,13 @@
+﻿namespace P3
+{
+    public interface IObstacle
+    {
+        bool PursueTaxi { get; }
+        bool isSolid { get; }
+        int Damage { get; }
+        double SpeedMultiplier { get; }
+        int EffectDuration { get; }
+        void ApplyEffect(Taxi taxi);
+
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,59 @@
+﻿namespace P3
+{
+    public class Taxi
+    {
+        private static Taxi? _instance; // Singleton instance
+        private int _life;
+        private double _speed;
+        private int _lastLifeValue;
+        private double _lastSpeedValue;
+
+        private Taxi()
+        {
+            _life = 100;
+            _speed = 1;
+            _lastLifeValue = _life;
+            _lastSpeedValue = _speed;
+        }
+
+        public static Taxi Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Taxi();
+                }
+
+                return _instance;
+            }
+        }
+
+        public void ApplyObstacle(IObstacle obstacle)
+        {
+            _lastLifeValue = _life;
+            _lastSpeedValue = _speed;
+
+            _life -= obstacle.Damage;
+            _speed *= obstacle.SpeedMultiplier;
+
+            Console.WriteLine(ToString());
+        }
+
+        public void RestoreSpeed(double originalSpeed)
+        {
+            _lastSpeedValue = _speed;
+
+            _speed = originalSpeed;
+
+            Console.WriteLine(ToString());
+        }
+
+        public override string ToString()
+        {
+            return $"Life: {_lastLifeValue} -> {_life}, Speed: {_lastSpeedValue} -> {_speed}";
+        }
+
+
+    }
+}
